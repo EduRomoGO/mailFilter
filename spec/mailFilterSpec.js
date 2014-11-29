04886d1f06c6c2dfdb99a7ac02f6f22f3ecad170
EduRomoGO <eduromogo@outlook.com>
2014-11-29 22:20:58 +0100

    first commit

---
+++ spec/mailFilterSpec.js
@@ -0,0 +1,16 @@
+
+/*
+TODO LIST
+   f([]) -> []
+   f(['a@a.c']) -> ['a@a.c']
+   f(['ab@ab.c']) -> ['ab@ab.c']
+   f(['@ac']) -> 'The input array did not contain any valid email'
+   f(['@ac','s']) -> 'The input array did not contain any valid email'
+   f(['.ac','s']) -> 'The input array did not contain any valid email'
+   f(['a@a.c','s']) -> ['a@a.c'] 
+*/
+
+
+describe("The mail filter", function(){
+
+});
