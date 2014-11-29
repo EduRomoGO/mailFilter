5c3a8fbd725129a39844461fe9ba53c165769aaf
EduRomoGO <eduromogo@outlook.com>
2014-11-30 00:29:31 +0100

    4 specs green

--- spec/mailFilterSpec.js
+++ spec/mailFilterSpec.js
@@ -13,4 +13,20 @@ TODO LIST
 
 describe("The mail filter", function(){
 
+  it("should return an empty list, if receive and empty list to filter", function(){
+      expect(filter([])).toEqual([]);
+  });
+
+  it("should return an empty list, if receive a mail without an @ symbol", function(){
+      expect(filter(['a'])).toEqual([]);
+  });
+
+  it("should return an empty list, if receive a mail without a . symbol", function(){
+      expect(filter(['a@'])).toEqual([]);
+  });
+
+  it("should return an empty list, if receive a mail where the . comes before the @", function(){
+      expect(filter(['a.@'])).toEqual([]);
+  });
+
 });
--- src/mailFilter.js
+++ src/mailFilter.js
@@ -1,4 +1,10 @@
 
 function filter (emailCandidates) {
+	if (emailCandidates.length === 0 						||
+			emailCandidates[0].indexOf('@') === -1	||
+			emailCandidates[0].indexOf('.') === -1	||
+			emailCandidates[0].indexOf('.') < emailCandidates[0].indexOf('@'))
+		return [];
 
+	return emailCandidates;
 }
