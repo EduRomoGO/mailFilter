39d58cb49ff29ca19305feedbd5b183729454271
EduRomoGO <eduromogo@outlook.com>
2014-11-30 01:18:15 +0100

    changes in todo list

--- spec/mailFilterSpec.js
+++ spec/mailFilterSpec.js
@@ -4,12 +4,11 @@ TODO LIST
    f([]) -> []
    f(['a@a.c']) -> ['a@a.c']
    f(['ab@ab.c']) -> ['ab@ab.c']
-   f(['@ac']) -> 'The input array did not contain any valid email'
-   f(['@ac','s']) -> 'The input array did not contain any valid email'
-   f(['.ac','s']) -> 'The input array did not contain any valid email'
+   f(['@ac']) -> []
+   f(['@ac','s']) -> []
+   f(['.ac','s']) -> []
    f(['a@a.c','s']) -> ['a@a.c'] 
 */
-
 
 describe("The mail filter", function(){
 
